18e54b79e86657887ea2a5e06821218be14ebc70
luxinglong <154290493@qq.com>
2017-12-28 23:25:05 +0800

    maxSubsequenceSum

---
+++ maxSubsequenceSum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+// method one which can achieve O(N^3)
+
+int maxSubsequenceSum1(int a[], int size, int &start, int &end)
+{
+	int maxSum = 0;
+	for (int i = 0; i < size; i++)
+		for (int j = i; j < size; j++)
+		{
+			
+		}
+	return maxSum;
+}
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
